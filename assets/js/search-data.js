17bb833dd2d4f5f41e32e15d2196460f1d13a65b
kziliask <110923646+kziliask@users.noreply.github.com>
2025-08-13 21:00:28 +0000

    Deploying to gh-pages from @ kziliask/kziliask.github.io@06cd725b7df1255747342351fee3cbaa710d815b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -46,7 +46,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "My teaching experience and courses taken.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
@@ -501,35 +501,28 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6B%7A%69%6C%69%61%73%6B%6F%70%6F%75%6C%6F%73@%70%72%6F%74%6F%6E%6D%61%69%6C.%63%68", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/kziliask", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/kziliask", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=WAae1qgAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
