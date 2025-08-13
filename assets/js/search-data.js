4740d64c11d076e8efc599097b50f5039a2f8df9
kziliask <110923646+kziliask@users.noreply.github.com>
2025-08-13 21:52:31 +0000

    Deploying to gh-pages from @ kziliask/kziliask.github.io@e914252812cf758eacb93c351ecd42b2e016f236 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,8 +44,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
+        },{id: "nav-courses",
+          title: "courses",
           description: "My teaching experience and courses taken.",
           section: "Navigation",
           handler: () => {
@@ -58,21 +58,14 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/people/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "nav-submenus",
+          title: "submenus",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/_pages/dropdown/";
+          },
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
